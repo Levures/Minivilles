77c4efd48c0ca3f8476c0e61abe735397d08199c
Natsunkyun <elodie.prudent@outlook.fr>
2021-10-26 17:37:08 +0200

    Push de fin de journée

--- Minivilles/Game.cs
+++ Minivilles/Game.cs
@@ -11,9 +11,40 @@ namespace Minivilles
         public Pile[] piles;
         public Player[] players;
         private bool playerTurn;
+        public bool endGame = false;
         public Display display;
 
+        public Game game()
+        {
+            Console.WriteLine("Bienvenue dans Minivilles !");
 
+            while (!endGame)
+            {
+                // Début du tour du joueur
+                playerTurn = true;
+                Console.WriteLine("Tour du joueur :");
+                display.DisplayCards();
+                Console.WriteLine("Appuyez sur Entrée pour lancer le dé.");
+                Console.ReadLine();
+
+                //Le joueur lance le dé
+                players[0].die.Roll();
+                if (playerTurn)
+                {
+                    
+                    
+                }
+                else
+                {
+
+                }
+
+                //Le joueur achète ou non une propriété
+            }
+            
+
+            return game();
+        }
 
     }
 }
