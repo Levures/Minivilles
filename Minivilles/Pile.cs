265c3578032df15ba151c75fb82e5276c0db68cd
Jacottin <49026472+Jacottin@users.noreply.github.com>
2021-10-26 15:37:25 +0200

    Upgraded Pile class (also known as ULTRA PILE CLASS NOW)

--- Minivilles/Pile.cs
+++ Minivilles/Pile.cs
@@ -2,7 +2,13 @@
 {
     public class Pile
     {
-        public Card card;
-        public int nbCards;
+        public Card card { get; }
+        public int nbCards { get; }
+
+        public int WithdrawCard()
+        {
+            int withdrawal = nbCards - 1;
+            return withdrawal;
+        }
     }
 }
