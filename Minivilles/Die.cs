33550b7dc2ba5cbf182cd9e3ca96ab9fb8bef373
Natsunkyun <elodie.prudent@outlook.fr>
2021-10-26 15:25:22 +0200

    Ajout classe Die

---
+++ Minivilles/Die.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minivilles
+{
+    public class Die
+    {
+        private Random random = new Random();
+        public int nbFace;
+
+        public Die(int nbFace)
+        {
+            this.nbFace = nbFace;
+        }
+
+        public int Roll()
+        {
+            int currentFace = random.Next(1, nbFace + 1);
+            return currentFace;
+        }
+    }
+}
+
+
