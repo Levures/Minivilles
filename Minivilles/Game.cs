4cbfd715f95a2313ea4ca51dd9f0f5adad3e2660
Victor <vsenneville@yahoo.fr>
2021-10-28 10:31:21 +0200

    Effet des cartes fonctionnelles

--- Minivilles/Game.cs
+++ Minivilles/Game.cs
@@ -10,7 +10,6 @@ namespace Minivilles
     {
         public Pile[] piles;
         public Player[] players = new Player[2];
-        public bool playerTurn { get; private set; }
         public bool endGame = false;
         public Display display = new Display();
         private Random random = new Random();
@@ -40,11 +39,13 @@ namespace Minivilles
             players[0] = new Player();
             players[1] = new Player();
 
+            Console.WriteLine("{0}, {1}", players[0].town[0].GetCardName, players[0].town[1].GetCardName);
+
             while (!endGame)
             {
 
                 // Début du tour du joueur.
-                playerTurn = true;
+                players[0].isMyTurn = true;
                 Console.WriteLine("Tour du joueur:");
                 Console.WriteLine("Appuyez sur Entrée pour lancer le dé.");
                 Console.ReadLine();
@@ -53,6 +54,8 @@ namespace Minivilles
                 int dieFace = players[0].die.Roll();
                 display.DisplayDie(dieFace);
                 players[0].ApplyCardsEffect(dieFace);
+                players[1].ApplyCardsEffect(dieFace);
+                Console.WriteLine("le joueur a {0} pièce", players[0].coins.ToString());
 
                 //Le joueur achète ou non une propriété.
                 Console.WriteLine("Souhaitez-vous acheter une carte ?");
@@ -71,11 +74,16 @@ namespace Minivilles
 
                 //- Si non, la suite.
 
-
+                players[0].isMyTurn = false;
                 // Début du tour de l'ordinateur.
-                IATurn();
-                
-                
+                Console.WriteLine("Tour de l'ordinateur:");
+                players[1].isMyTurn = true;
+                System.Threading.Thread.Sleep(1000);
+                dieFace = players[1].die.Roll();
+                display.DisplayDie(dieFace);
+                players[0].ApplyCardsEffect(dieFace);
+                players[1].ApplyCardsEffect(dieFace);
+                IATurn();                
             }
 
             return game();
@@ -84,22 +92,19 @@ namespace Minivilles
         #region Private Methods
         private void IATurn()
         {
-            Console.WriteLine("Tour de l'ordinateur:");
-            System.Threading.Thread.Sleep(1000);
-            int iaDieFace = players[1].die.Roll();
-            display.DisplayDie(iaDieFace);
-
             int iaChoice;
             iaChoice = random.Next(2);
             List<Card> possibility = new List<Card>();
 
             List<Card> haveEnoughCoinToBuy = new List<Card>();
+            bool canBuyCard = false;
 
             foreach (Card entry in possibility)
             {
                 if (entry.GetCardCost >= players[1].coins)
                 {
                     haveEnoughCoinToBuy.Add(entry);
+                    canBuyCard = true;
                 }
             }
 
@@ -108,14 +113,23 @@ namespace Minivilles
             switch (iaChoice)
             {
                 case 0:
-                    Card iaChosenCard = haveEnoughCoinToBuy[random.Next(haveEnoughCoinToBuy.Count + 1)];
-                    players[1].BuyCard(iaChosenCard);
-                    Console.WriteLine("L'IA a choisi : {0}", iaChosenCard.GetCardName);
+                    if(canBuyCard)
+                    {
+                        Card iaChosenCard = haveEnoughCoinToBuy[random.Next(haveEnoughCoinToBuy.Count)];
+                        players[1].BuyCard(iaChosenCard);
+                        Console.WriteLine("L'IA a choisi : {0}", iaChosenCard.GetCardName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ordi : Pas de carte pour moi");
+                    }
                     break;
                 case 1:
                     Console.WriteLine("Ordi : Pas de carte pour moi");
                     break;
             }
+
+            players[1].isMyTurn = false;
         }
 
         #endregion
