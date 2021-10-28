0f8e3816f7cbd02bf3516069c643f6a311f2cb5d
Victor <vsenneville@yahoo.fr>
2021-10-28 10:03:22 +0200

    Création de la méthode IATurn()

--- Minivilles/Game.cs
+++ Minivilles/Game.cs
@@ -66,51 +66,59 @@ namespace Minivilles
                     //display.ChooseCard();
                 }
 
-                        //- Si oui, curseur sous les piles de carte.
-                    //players[0].BuyCard();
+                //- Si oui, curseur sous les piles de carte.
+                //players[0].BuyCard();
 
-                        //- Si non, la suite.
+                //- Si non, la suite.
 
 
                 // Début du tour de l'ordinateur.
-                Console.WriteLine("Tour de l'ordinateur:");
-                System.Threading.Thread.Sleep(1000);
-                dieFace = players[1].die.Roll();
-                display.DisplayDie(dieFace);
-
-                int iaChoice;
-                iaChoice = random.Next(2);
-                List<Card> possibility = new List<Card>();
-
-                List<Card> haveEnoughCoinToBuy = new List<Card>();
-
-                foreach(Card entry in possibility)
-                {
-                    if(entry.GetCardCost >= players[1].coins)
-                    {
-                        haveEnoughCoinToBuy.Add(entry);
-                    }
-                }
-
+                IATurn();
                 
-
-                switch (iaChoice)
-                {
-                    case 0:
-                        Card iaChosenCard = haveEnoughCoinToBuy[random.Next(haveEnoughCoinToBuy.Count + 1)];
-                        players[1].BuyCard(iaChosenCard);
-                        Console.WriteLine("L'IA a choisi : {0}", iaChosenCard.GetCardName);
-                        break;
-                    case 1:
-                        Console.WriteLine("Ordi : Pas de carte pour moi");
-                        break;
-                }                
                 
             }
 
-
             return game();
         }
+
+        #region Private Methods
+        private void IATurn()
+        {
+            Console.WriteLine("Tour de l'ordinateur:");
+            System.Threading.Thread.Sleep(1000);
+            int iaDieFace = players[1].die.Roll();
+            display.DisplayDie(iaDieFace);
+
+            int iaChoice;
+            iaChoice = random.Next(2);
+            List<Card> possibility = new List<Card>();
+
+            List<Card> haveEnoughCoinToBuy = new List<Card>();
+
+            foreach (Card entry in possibility)
+            {
+                if (entry.GetCardCost >= players[1].coins)
+                {
+                    haveEnoughCoinToBuy.Add(entry);
+                }
+            }
+
+
+
+            switch (iaChoice)
+            {
+                case 0:
+                    Card iaChosenCard = haveEnoughCoinToBuy[random.Next(haveEnoughCoinToBuy.Count + 1)];
+                    players[1].BuyCard(iaChosenCard);
+                    Console.WriteLine("L'IA a choisi : {0}", iaChosenCard.GetCardName);
+                    break;
+                case 1:
+                    Console.WriteLine("Ordi : Pas de carte pour moi");
+                    break;
+            }
+        }
+
+        #endregion
 
     }
 }
